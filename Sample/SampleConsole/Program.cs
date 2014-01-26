7fd94964de8259efac52225edb34c308854b46bb
Daniel Bohlin <daniel.bohlin@gmail.com>
2014-01-26 17:30:07 +0100

    Created a new simple sample console application

---
+++ Sample/SampleConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Tharga.Toolkit.Console;
+using Tharga.Toolkit.Console.Command;
+using Tharga.Toolkit.Console.Command.Base;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = new RootCommand();
+            command.RegisterCommand(new SomeContainerCommand());
+            new CommandEngine(command).Run(args);
+        }
+    }
+
+    class SomeContainerCommand : ContainerCommandBase
+    {
+        public SomeContainerCommand() 
+            : base("some")
+        {
+            RegisterCommand(new SomeListCommand());
+        }
+    }
+
+    class SomeListCommand : ActionCommandBase
+    {
+        public SomeListCommand() 
+            : base("list", "lists some information")
+        {
+
+        }
+
+        public override async Task<bool> InvokeAsync(string paramList)
+        {
+            for (var i = 0; i < 5; i++)
+                OutputInformation("Some data {0}", i);
+            return true;
+        }
+    }
+}
